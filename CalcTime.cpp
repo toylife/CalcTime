997af62ebbaf6f3bdbc3752c86dd8c5fc88fd279
toylife <timbers@qq.com>
2023-11-20 17:12:27 +0800

    add prompt

--- CalcTime.cpp
+++ CalcTime.cpp
@@ -11,22 +11,26 @@ int main()
     int hour3, minute3;
     int hour4, minute4;
 // 输入第1个时间段
+    printf("请输入第1次打卡的时间：");
     scanf("%d %d", &hour1, &minute1);
+    printf("请输入第2次打卡的时间：");
     scanf("%d %d", &hour2, &minute2);
-    printf("第1次：");
+    printf("第1次时长：");
     int t1 = Duration(hour1, minute1, hour2, minute2);
 
 
 // 输入第2个时间段
+    printf("请输入第3次打卡的时间：");
     scanf("%d %d", &hour3, &minute3);
+    printf("请输入第4次打卡的时间：");
     scanf("%d %d", &hour4, &minute4);
-    printf("第2次：");
+    printf("第2次时长：");
     int t2 = Duration(hour3, minute3, hour4, minute4);
 
 
 // 打印总时长
     t2 = t1 + t2;
-    printf("总计：");
+    printf("总计时长：");
     PrintDuration(t2);
     return 0;
 }
