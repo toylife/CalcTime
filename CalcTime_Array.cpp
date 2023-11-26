e6245359224937e99207839a63b0e45985037754
toylife <timbers@qq.com>
2023-11-26 23:11:12 +0800

    display every turn and accumulative time

--- CalcTime_Array.cpp
+++ CalcTime_Array.cpp
@@ -2,14 +2,17 @@
 #include <math.h>
 #include <string.h>
 
+struct Time
+{
+    int hour;
+    int minute;
+} times[8];
+
+int CalcTime(struct Time, int i);
+
 int main()
 {
     // 定义打卡时间数组
-    struct Time
-    {
-        int hour;
-        int minute;
-    } times[8];
 
     char temp[4];
     int total_time = 0;
@@ -37,15 +40,27 @@ int main()
             printf("OK.\n---------\n");
             break;
         }
+        if ((i + 1) % 2 == 0)
+        {
+            static int time_Accumulate;
+            int time = CalcTime(times[i], i);
+            time_Accumulate+=CalcTime(times[i],i);
+            printf("---------\n本次工作时间：\t%d 时 %2d 分\n", time / 60, time % 60);
+            if(i>1){
+                printf("前%d次累计时间：\t%d 时 %2d 分\n",i+1,time_Accumulate / 60, time_Accumulate % 60);
+            }
+            printf("---------\n");
+        }
+        if((i+1)%2==0){
+            
+        }
     }
     // 计算工作时间总数
 
     for (int i = 1; i < punch_no; i += 2)
     {
         // 计算两次打卡之间的时间差
-        int diff1 = times[i].hour * 60 + times[i].minute;
-        int diff2 = times[i - 1].hour * 60 + times[i - 1].minute;
-        total_time += fabs(diff2 - diff1);
+        total_time += CalcTime(times[i], i);
     }
 
     // 输出工作时间总数
@@ -53,4 +68,11 @@ int main()
     printf("工作时间：\t%d 时 %d 分\n", total_time / 60, total_time % 60);
 
     return 0;
+}
+
+int CalcTime(struct Time, int i)
+{
+    int diff1 = times[i].hour * 60 + times[i].minute;
+    int diff2 = times[i - 1].hour * 60 + times[i - 1].minute;
+    return (fabs(diff2 - diff1));
 }
